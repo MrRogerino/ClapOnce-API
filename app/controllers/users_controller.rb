ea9b7ed2437e4438d98ca7e9db1ea73ed1c61608
Roger Li <email": null>
2017-07-29 23:11:40 -0700

    Some shit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  EARTH_RADIUS = 3961
 
   def index
     @users = []
@@ -37,14 +38,42 @@ class UsersController < ApplicationController
 
   private
 
-  def distance(epicenter,user_location)
-    p user_location
-    delta_x = (epicenter[0]-user_location[0])
-    delta_y = (epicenter[1]-user_location[1])
-    return Math.sqrt(delta_y**2 + delta_x**2)
+  def alert_users(epicenter, severity)
+    affected_users = []
+    User.find_each do |user|
+      if distance(epicenter, user.location).
+  end
+
+  def radius_affected(severity)
+    case severity
+    when "moderate"
+      return 3
+    when "severe"
+      return 5
+    end
+  end
+
+
+  def distance(epicenter,user_location) #gets distance between two points in miles
+    d_longitude = toRadians((epicenter[1]-user_location[1]))
+    d_latitude = toRadians((epicenter[0]-user_location[0]))
+
+    sin_d_long = Math.sin(d_longitude)
+    sin_d_lat = Math.sin(d_latitude)
+
+    rad_epi_lat = toRadians(epicenter[0])
+    rad_user_lat = toRadians(epicenter[1])
+
+    a = sin_d_lat ** 2 + Math.cos(rad_epi_lat) * Math.cos(rad_user_lat) * (sin_d_long ** 2)
+    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+    return EARTH_RADIUS * c
   end
 
   def change_status(user, status)
     user.status = status
+  end
+
+  def toRadians(degree)
+    return degrees * Math::PI / 180
   end
 end
